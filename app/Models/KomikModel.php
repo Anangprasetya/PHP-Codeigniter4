2e271441fc4382fae9eccee36c4ba83d0a68fe44
Anang <tyoandini@gmail.com>
2021-05-12 09:12:02 +0700

    insert data

--- app/Models/KomikModel.php
+++ app/Models/KomikModel.php
@@ -8,14 +8,15 @@ class KomikModel extends Model
 {
     protected $table = 'komik';
     protected $useTimestamps = true;
+    protected $allowedFields = ['judul', 'slug', 'penulis', 'penerbit', 'sampul'];
 
 
     public function getKomik($slug = false)
     {
         if ($slug == false) {
             return $this->findAll();
+        } else {
+            return $this->where(['slug' => $slug])->first();
         }
-
-        return $this->where(['slug' => $slug])->first();
     }
 }
---
+++ database/komik.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 12, 2021 at 09:09 AM
+-- Server version: 8.0.23-0ubuntu0.20.04.1
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ci4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `komik`
+--
+
+CREATE TABLE `komik` (
+  `id` int NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `penulis` varchar(255) NOT NULL,
+  `penerbit` varchar(255) NOT NULL,
+  `sampul` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `komik`
+--
+
+INSERT INTO `komik` (`id`, `judul`, `slug`, `penulis`, `penerbit`, `sampul`, `created_at`, `updated_at`) VALUES
+(1, 'Naruto', 'naruto', 'Masshi Kisimoto', 'Shonencham', 'naruto.jpg', NULL, NULL),
+(2, 'Kapten Subasa', 'kapten-subasa', 'Odara Taichi', 'Gramedia', 'subasa.jpg', NULL, NULL),
+(3, 'Eye Shield 21', 'eye-shield-21', 'Anang', 'Gramedia', 'asas', '2021-05-11 20:46:22', '2021-05-11 20:46:22'),
+(4, 'ss', 'ss', 'aa', 'a', 'asas', '2021-05-11 21:05:15', '2021-05-11 21:05:15'),
+(5, '', '', '', '', '', '2021-05-11 21:05:57', '2021-05-11 21:05:57');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `komik`
+--
+ALTER TABLE `komik`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `komik`
+--
+ALTER TABLE `komik`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
