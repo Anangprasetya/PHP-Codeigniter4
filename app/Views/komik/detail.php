ff82446cb2f7233ebf19d1da967d507367f25f70
Anang <tyoandini@gmail.com>
2021-05-17 08:49:58 +0700

    delete dan edit

--- app/Views/komik/detail.php
+++ app/Views/komik/detail.php
@@ -15,8 +15,14 @@
                             <h5 class="card-title"><?php echo $komik['judul']; ?></h5>
                             <p class="card-text"><b>Penulis : <?php echo $komik['penulis']; ?></b></p>
                             <p class="card-text"><small class="text-muted"><b>Penerbit : <?php echo $komik['penerbit']; ?></b></small></p>
-                            <a href="" class="btn btn-warning">Edit</a>
-                            <a href="" class="btn btn-danger">Delete</a>
+                            <a href="/komik/edit/<?php echo $komik['slug']; ?>" class="btn btn-warning">Edit</a>
+
+                            <form action="/komik/<?php echo $komik['id']; ?>" method="POST" class="d-inline">
+                                <?php echo csrf_field(); ?>
+                                <input type="hidden" name="_method" value="DELETE">
+                                <button type="submit" class="btn btn-danger" onclick="return confirm('Apakah anda yakin ?')">Delete</button>
+                            </form>
+
                             <br><br>
                             <a href="/komik">Kembali</a>
                         </div>
---
+++ app/Views/komik/edit.php
@@ -0,0 +1,49 @@
+<?php echo $this->extend('layout/template'); ?>
+
+<?php echo $this->section('content'); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+            <h2 class="my-3"><?php echo $title; ?></h2>
+            <form action="/komik/update/<?php echo $komik['id']; ?>" method="POST">
+                <?php echo csrf_field(); ?>
+                <input type="hidden" name="slug" value="<?php echo $komik['slug']; ?>">
+                <div class="form-group row">
+                    <label for="judul" class="col-sm-2 col-form-label">Judul</label>
+                    <div class="col-sm-10 my-3">
+                        <input type="text" class="form-control <?= ($validation->hasError('judul')) ? 'is-invalid' : ''; ?>" id="judul" name="judul" autofocus value="<?= (old('judul')) ? old('judul') : $komik['judul']; ?>">
+                        <div class="invalid-feedback">
+                            <?php echo $validation->getError('judul'); ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="penulis" class="col-sm-2 col-form-label">Penulis</label>
+                    <div class="col-sm-10 my-3">
+                        <input type="text" class="form-control" id="penulis" name="penulis" value="<?= (old('penulis')) ? old('penulis') : $komik['penulis']; ?>">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="penerbit" class="col-sm-2 col-form-label">Penerbit</label>
+                    <div class="col-sm-10 my-3">
+                        <input type="text" class="form-control" id="penerbit" name="penerbit" value="<?= (old('penerbit')) ? old('penerbit') : $komik['penerbit']; ?>">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
+                    <div class="col-sm-10 my-3">
+                        <input type="text" class="form-control" id="sampul" name="sampul" value="<?= (old('sampul')) ? old('sampul') : $komik['sampul']; ?>">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-10 my-3">
+                        <button type="submit" class="btn btn-primary">Ubah Data</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php echo $this->endSection(); ?>
