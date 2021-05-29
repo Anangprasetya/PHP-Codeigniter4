e51fe559bf9756d7de5436f27d02e6763dbabc6c
Anang <tyoandini@gmail.com>
2021-05-29 14:24:11 +0700

    faker

--- app/Database/Seeds/OrangSeeder.php
+++ app/Database/Seeds/OrangSeeder.php
@@ -9,32 +9,46 @@ class OrangSeeder extends Seeder
 {
     public function run()
     {
-        $data = [
-            [
-                'nama' => 'Anang Nur Prasetya',
-                'alamat'    => 'Mojokerto',
-                'created_at' => Time::now(),
+        // $data = [
+        //     [
+        //         'nama' => 'Anang Nur Prasetya',
+        //         'alamat'    => 'Mojokerto',
+        //         'created_at' => Time::now(),
+        //         'updated_at' => Time::now(),
+        //     ],
+        //     [
+        //         'nama' => 'Doddy Ferdiansyah',
+        //         'alamat'    => 'Lampung',
+        //         'created_at' => Time::now(),
+        //         'updated_at' => Time::now(),
+        //     ],
+        //     [
+        //         'nama' => 'Erik',
+        //         'alamat'    => 'Banjar',
+        //         'created_at' => Time::now(),
+        //         'updated_at' => Time::now(),
+        //     ]
+        // ];
+
+        $faker = \Faker\Factory::create('id_ID');
+
+        for ($i = 0; $i < 100; $i++) {
+            $data = [
+                'nama' => $faker->name,
+                'alamat'    => $faker->address,
+                'created_at' => Time::createFromTimestamp($faker->unixTime()),
                 'updated_at' => Time::now(),
-            ],
-            [
-                'nama' => 'Doddy Ferdiansyah',
-                'alamat'    => 'Lampung',
-                'created_at' => Time::now(),
-                'updated_at' => Time::now(),
-            ],
-            [
-                'nama' => 'Erik',
-                'alamat'    => 'Banjar',
-                'created_at' => Time::now(),
-                'updated_at' => Time::now(),
-            ]
-        ];
+
+            ];
+            $this->db->table('orang')->insert($data);
+        }
+
+
 
         // Simple Queries
         // $this->db->query("INSERT INTO orang (nama, alamat, created_at, updated_at) VALUES(:nama:, :alamat:, :created_at:, :updated_at:)", $data);
 
         // Using Query Builder
-        // $this->db->table('orang')->insert($data);
-        $this->db->table('orang')->insertBatch($data);
+        // $this->db->table('orang')->insertBatch($data);
     }
 }
