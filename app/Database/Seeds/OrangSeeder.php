57588396056907a262611243fb5f950019f021ab
Anang <tyoandini@gmail.com>
2021-05-29 13:50:25 +0700

    seeder

---
+++ app/Database/Seeds/OrangSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use CodeIgniter\I18n\Time;
+
+class OrangSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            [
+                'nama' => 'Anang Nur Prasetya',
+                'alamat'    => 'Mojokerto',
+                'created_at' => Time::now(),
+                'updated_at' => Time::now(),
+            ],
+            [
+                'nama' => 'Doddy Ferdiansyah',
+                'alamat'    => 'Lampung',
+                'created_at' => Time::now(),
+                'updated_at' => Time::now(),
+            ],
+            [
+                'nama' => 'Erik',
+                'alamat'    => 'Banjar',
+                'created_at' => Time::now(),
+                'updated_at' => Time::now(),
+            ]
+        ];
+
+        // Simple Queries
+        // $this->db->query("INSERT INTO orang (nama, alamat, created_at, updated_at) VALUES(:nama:, :alamat:, :created_at:, :updated_at:)", $data);
+
+        // Using Query Builder
+        // $this->db->table('orang')->insert($data);
+        $this->db->table('orang')->insertBatch($data);
+    }
+}
